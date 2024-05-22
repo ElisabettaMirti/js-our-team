583570ec0d111f7d96199d5494c71ee538cf7e58
ElisabettaMirti <belmirti@gmail.com>
2024-05-22 17:01:35 +0200

    aggiunto array di oggetti e stampato in console con ciclo for-in

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const ourTeam = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\walter-gordon-office-manager.jpg"
+    },
+
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\scott-estrada-developer.jpg"
+    },
+
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+for (let key in ourTeam) {
+    console.log(ourTeam[key]);
+}
