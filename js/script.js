af64dd5694d6ccb002435ee56caf1c0e410559af
ElisabettaMirti <belmirti@gmail.com>
2024-05-23 08:55:16 +0200

    corretti url delle foto o_o

--- js/script.js
+++ js/script.js
@@ -2,37 +2,37 @@ const ourTeam = [
     {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\wayne-barnett-founder-ceo.jpg"
+        foto: "./img/wayne-barnett-founder-ceo.jpg"
     },
 
     {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
-        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\angela-caroll-chief-editor.jpg"
+        foto: "./img/angela-caroll-chief-editor.jpg"
     },
 
     {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
-        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\walter-gordon-office-manager.jpg"
+        foto: "./img/walter-gordon-office-manager.jpg"
     },
 
     {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
-        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\angela-lopez-social-media-manager.jpg"
+        foto: "./img/angela-lopez-social-media-manager.jpg"
     },
 
     {
         nome: "Scott Estrada",
         ruolo: "Developer",
-        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\scott-estrada-developer.jpg"
+        foto: "./img/scott-estrada-developer.jpg"
     },
 
     {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
-        foto: "C:\Users\Elisabetta\Desktop\Boolean\js-our-team\img\barbara-ramos-graphic-designer.jpg"
+        foto: "./img/barbara-ramos-graphic-designer.jpg"
     }
 ]
 
